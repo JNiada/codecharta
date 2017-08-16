2ad513af1e7934ec94e5f2e4517c5cf96ab50e66
mrpatpat <adrian.endrich@googlemail.com>
2017-08-16 22:16:07 +0200

    3) adding basic src folder

---
+++ visualization/src/routes.js
@@ -0,0 +1,19 @@
+export default routesConfig;
+
+/** @ngInject */
+function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('app', {
+      url: '/',
+      component: 'hello'
+    });
+
+  $stateProvider
+    .state('hello', {
+      url: '/hello',
+      component: 'hello'
+    });
+}
